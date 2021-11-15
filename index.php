5bb9bca13086ebc19c48a793f93882dcde470a23
houdret <jeanhoudret2006@hotmail.com>
2021-11-15 14:37:41 +0100

    index.php

--- index.php
+++ index.php
@@ -10,19 +10,23 @@
 </head>
 <body>
     <div class="container mt-5">
+    <div class="col-12 ">        
+            <img src="./asset/img/moi.png" alt="moi" class="rounded float-end p-2" srcset="moi.png 30w 30h, moi.png 50w 200h"
+            sizes="(max-width: 200px) 200px, 5vw" >
+        </div>
         <form action="" method="post" class=" was-validated row g-1 ">
             <label for="firstname" class="form-label">Firstname </label> 
             <input type="text" name="firstname" class="form-control is-valid" required>
             <label for="lastname" class="form-label">Lastname </label> 
             <input type="text" name="lastname" class="form-control is-valid" required>            
-            <label for="password" class="form-label">Password </label> 
-            <input type="password" class="form-control is-valid" placeholder="Password" required>            
+            <label for="gender" class="form-label">Gender </label> 
+            <input type="gender" class="form-control is-valid" required>            
             <label for="email" class="form-label">Email </label> 
             <input type="email" name="email" class="form-control is-valid" required>
-            <label for="tel" class="form-label">Tel </label> 
-            <input type="tel" name="tel" class="form-control is-valid" required>
-            <label for="url" class="form-label">Site Web </label> 
-            <input type="url" name="url" class="form-control">
+            <label for="company" class="form-label">Company </label> 
+            <input type="text" name="company" class="form-control is-valid" required>
+            <label for="subject" class="form-label" >Subject </label> 
+            <input type="text" name="subject" class="form-control" required>
             <div class="mb-3">
                 <label for="message" class="form-label">Message </label> 
                 <textarea class="form-control is-invalid" rows="5" cols="40" placeholder="Required example textarea" name="message" required></textarea>
@@ -36,62 +40,81 @@
         </form>
         <?php
         error_reporting(E_ALL ^ E_WARNING);
+
         if (isset($_POST['submit'])) {
-            /* $options = array(
-                'firstname'       => FILTER_SANITIZE_STRING,
-                'lastname'        => FILTER_SANITIZE_STRING,
-                'email'           => FILTER_SANITIZE_EMAIL,
-                'tel'             => FILTER_SANITIZE_NUMBER_INT,
-                'url'             => FILTER_SANITIZE_URL,
-                'message'         => FILTER_SANITIZE_STRING
-            );
-            $result = filter_input_array(INPUT_POST, $options); */
-            // 1. Sanitisation
-            $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-
-            $firstname = filter_var(
-                $_POST['firstname'],
-                FILTER_SANITIZE_STRING
-            );
-
-            $password = filter_var(
-                $_POST['password'],
-                FILTER_SANITIZE_STRING
-            );
+                       
+            $firstname = filter_var($_POST['firstname'], FILTER_SANITIZE_STRING);
+            $lastname = filter_var($_POST['lastname'], FILTER_SANITIZE_STRING);
+            $gender = filter_var($_POST['gender'],FILTER_SANITIZE_STRING);
+            $email = filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
+            $company = filter_var($_POST['company'],FILTER_SANITIZE_STRING);
+            $subject = filter_var($_POST['subject'],FILTER_SANITIZE_STRING);
+            $commentaire = filter_var($_POST['message'],FILTER_SANITIZE_STRING);
 
             if (false === filter_var($firstname, FILTER_SANITIZE_STRING)) {
                 $errors['firstname'] = 'This firstname is invalid.';
             } else {
+                echo 'votre prénom est bien nettoyé et est considérée comme valide.<hr>';
+            }
+            if (false === filter_var($firstname, FILTER_SANITIZE_STRING)) {
+                $errors['lastname'] = 'This lastname is invalid.';
+            } else {
                 echo 'votre nom est bien nettoyé et est considérée comme valide.<hr>';
             }
-            // 2. Validation
-            if (false === filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            if (false === filter_var($gender, FILTER_SANITIZE_STRING)) {
+                $errors['gender'] = 'This gender is invalid.';
+            } else {
+                echo 'votre genre est bien nettoyé et est considérée comme valide.<hr>';
+            }
+            if (false === filter_var($email, FILTER_SANITIZE_EMAIL)) {
+                $errors['email'] = 'This email is invalid.';
+            } else {
+                echo 'votre email est bien nettoyé et est considérée comme valide.<hr>';
+            }
+            if (false === filter_var($company, FILTER_SANITIZE_STRING)) {
+                $errors['company'] = 'This company is invalid.';
+            } else {
+                echo 'votre compagnie est bien nettoyé et est considérée comme valide.<hr>';
+            }
+            if (false === filter_var($subject, FILTER_SANITIZE_STRING)) {
+                $errors['subject'] = 'This subject is invalid.';
+            } else {
+                echo 'votre subjet est bien nettoyé et est considérée comme valide.<hr>';
+            }
+            if (false === filter_var($message, FILTER_SANITIZE_STRING)) {
+                $errors['message'] = 'This message is invalid.';
+            } else {
+                echo 'votre message est bien nettoyé et est considérée comme valide.<hr>';
+            }
+            
+            if (false === filter_var($email, FILTER_VALIDATE_STRING)) {
                 $errors['email'] = 'This address is invalid.';
             } else {
-                echo 'Cette adresse email nettoyée est considérée comme valide.<hr>';
+                echo 'Cette adresse email est considérée comme valide.<hr>';
             }
 
-            // 3. execution
-            /* if (count($errors)> 1){
+            
+            if (count($errors)> 1){
                 echo "There are mistakes!";
                 print_r($errors);
                 exit;
             }else {
                 echo 'Bravo vos données ont été bien encodées <hr>';
-            } */
+            }
+            
             $data = [
                 'firstname' => $_POST['firstname'],
                 'lastname' => $_POST['lastname'],
-                'password' => sha1($_POST['password']),
+                'gender' => ($_POST['gender']),
                 'email' => $_POST['email'],
-                'tel' => $_POST['tel'],
-                'url' => $_POST['url'],
+                'company' => $_POST['company'],
+                'subject' => $_POST['subject'],
                 'message' => $_POST['message'],
             ];
 
             $bdd = new PDO('mysql:host=localhost;dbname=test', 'root', '');
             $insertion =
-                'INSERT INTO table_essai (firstname, lastname, password, email, tel, url, message ) VALUES (:firstname,:lastname,:password,:email,:tel,:url,:message)';
+                'INSERT INTO portfolio (firstname, lastname, gender, email, company, subject, message ) VALUES (:firstname,:lastname,:gender,:email,:company,:subject,:message)';
             $bdd->prepare($insertion)->execute($data);
 
             // 4. Display the response interface.
